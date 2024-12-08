364f09c3f2c04d2c1d9675e03943e08c0dc9a4bc
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2024-12-08 20:49:34 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -2,16 +2,10 @@
 if [ "$(uname)" == "Darwin" ]; then
   mkdir -p $HOME/bin/.iython
   mv ./mac/iython $HOME/bin/.iython
-  CURRENT_DIR=$(basename $(pwd))
-  cd ..
-  rm -rf $CURRENT_DIR
 
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   mkdir -p $HOME/bin/.iython
   mv ./linux/iython $HOME/bin/.iython
-  CURRENT_DIR=$(basename $(pwd))
-  cd ..
-  rm -rf $CURRENT_DIR
 
 else
   echo "Not available on your OS"
